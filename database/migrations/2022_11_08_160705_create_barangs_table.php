1fc2507ba80cbe54031197c7400217293a00b971
WieElang <elangsurya66@gmail.com>
2022-11-09 23:56:44 +0700

    add tk3 init

---
+++ database/migrations/2022_11_08_160705_create_barangs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('deskripsi');
+            $table->string('jenis_barang');
+            $table->integer('stock_barang');
+            $table->double('harga_beli');
+            $table->double('harga_jual');
+            $table->string('gambar_barang')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
